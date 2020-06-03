9a15c79b68d6df968c6043aa5945de308d75a77e
dumoresco <eduardomorescoiost@gmail.com>
2020-06-03 14:54:08 -0300

    Dia 03

--- scripts/createPoint.js
+++ scripts/createPoint.js
@@ -24,11 +24,12 @@ function getCities(event){
     
     let url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
+    citySelect.innerHTML = "<option>Selecione a cidade</option>";
+    citySelect.disabled = true;
     fetch(url).then((res) => {
         return res.json().then( cities => { 
-
             for( let city of cities){
-                citySelect.innerHTML +=  `<option value="${city.id}">${city.nome}</option>`;
+                citySelect.innerHTML +=  `<option value="${city.nome}">${city.nome}</option>`;
             }
 
             citySelect.disabled = false
@@ -42,3 +43,54 @@ function getCities(event){
 
 document.querySelector("select[name = uf]").addEventListener("change", getCities)
 
+
+
+// Itens de Coleta 
+
+// Pegar todos os li's
+const itensToCollect = document.querySelectorAll(".itens-grid li");
+
+for(let itens of itensToCollect){
+    itens.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = [];
+
+
+function handleSelectedItem(event){
+    let itemLi = event.target;
+    // add or remove class with js
+    
+    itemLi.classList.toggle("selected");
+
+    let itemId = itemLi.dataset.id;
+
+    
+
+    // Verificar se existe items selecionados, se sim
+    // pegar os items selecionados
+
+    let alredySelected = selectedItems.findIndex( item =>{
+        let itemFound = item == itemId; //Isso será True or False
+        return itemFound
+    })
+    // se ja estiver selecionado, tirar da seleção
+    if(alredySelected >= 0){
+        let filteredItems = selectedItems.filter(item =>{
+            let itemsIsDifferent = item != itemId
+            return itemsIsDifferent
+        })
+        selectedItems = filteredItems;
+    }
+    else{
+        // se não estiver selecionado, adicionar na seleção
+        selectedItems.push(itemId);
+    }
+    
+    collectedItems.value = selectedItems
+    // atualizar o campo escondido com os dados selecionados
+
+
+}
---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+let btnSearch = document.querySelector("#page-home main a")
+let modal = document.querySelector("#modal")
+let close = document.querySelector("#modal .header a")
+
+btnSearch.addEventListener("click", () => {
+    modal.classList.remove("hide")
+})
+
+
+
+close.addEventListener("click", () =>{
+    modal.classList.add("hide")
+})
